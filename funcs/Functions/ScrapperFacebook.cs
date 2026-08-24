4f63792fe1add46ba4aaa3112cbec94fd9da8119
Vitalii Priadilia <vitalii.priadilia@fores.group>
2026-08-24 19:42:25 +0200

    first commit

---
+++ funcs/Functions/ScrapperFacebook.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace funcs.Functions;
+
+public class ScrapperFacebook(
+    ILogger<ScrapperFacebook> logger)
+{
+    [Function("ScrapperFacebook")]
+    public void Run(
+        [TimerTrigger("0 */5 * * * *")] 
+        TimerInfo myTimer)
+    {
+        
+    }
+}
